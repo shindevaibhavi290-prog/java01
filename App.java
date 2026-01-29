49342d7bda255de6b899a82c22029c302b9c0ac8
shindevaibhavi290-prog <shindevaibhavi290@gmail.com>
2026-01-29 18:44:23 +0530

    Add App.java with addition functionality

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class App {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Jenkins Java Demo Project");
+        System.out.println("Addition Result: " + add(5, 3));
+    }
+}
